6c73bcc75a0932671b8bab464273f6f65120aef7
Xavier <Xavier@Xavier-PC>
2015-02-02 20:17:30 +0100

    Ejemplos con  Buffered Streams en Java 7.

---
+++ BufferedStreams/src/bufferedstreams/TestCharactersStreams.java
@@ -0,0 +1,38 @@
+package bufferedstreams;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TestCharactersStreams {
+
+    public static void main(String[] args) {
+        
+        //  Try-with-resources para buffers de lectura/escritura de ficheros
+        try (FileReader input = new FileReader("src\\bufferedstreams\\file1_chars.txt");
+                FileWriter output = new FileWriter("src\\bufferedstreams\\file2_chars.txt")) {
+
+            //  Lectura de chars des del buffer de entrada
+            int charsRead;
+
+            //  Todos los caracteres leídos     
+            while ((charsRead = input.read()) != -1) {
+                
+                //  Se escriben los caracteres al buffer de salida...
+                output.write(charsRead);
+            }
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
